b8e84cb0f84e524ad069c4e4fac64abbed6c7fcd
Adrian Lupu <61655912+ansharai-web@users.noreply.github.com>
2021-08-05 15:50:52 +0300

    test
    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {TicTacToe} from './game/TicTacToe'
 
-
+function test() {
+return navigator.maxTouchPoints && navigator.maxTouchPoints > 1 ? "DA" :"NU
+}
 function App() {
   return (
-    <div >
+    <div>
+    {test()}
         <React.StrictMode>
             <TicTacToe/>
         </React.StrictMode>
